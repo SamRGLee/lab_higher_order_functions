23f7795b26c2448778eb957f86bb616e2b4de743
SamRGLee <sam_greenfield_lee@hotmail.co.uk>
2023-11-22 17:01:29 +0000

    completed mvp

--- ScranAdvisor.js
+++ ScranAdvisor.js
@@ -1,7 +1,28 @@
-const ScranAdvisor = function(restaurants){
+const ScranAdvisor = function(restaurants)
+{
     this.restaurants = restaurants;
 }
 
 // Add methods to prototype
+ScranAdvisor.prototype.noOfRestaurants = function ()
+{
+    return this.restaurants.length;
+}
+
+
+ScranAdvisor.prototype.findByName = function (name)
+{
+    return this.restaurants.find(restaurant => restaurant.name === name);
+}
+
+ScranAdvisor.prototype.allNames = function ()
+{
+    return this.restaurants.map(restaurant => restaurant.name);
+}
+
+ScranAdvisor.prototype.findAllByCity = function (name)
+{
+    return this.restaurants.filter(restaurant => restaurant.location.town === name);
+}
 
 module.exports = ScranAdvisor;
